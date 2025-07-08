9d5d4deafb9602cb3c6591e336c5b80b158a26af
nsikakessien <nsikakessien66@gmail.com>
2025-07-08 11:05:39 +0100

    remove key

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,3 @@
-// mongodb+srv://nsikakessien:Ofdesxu65ioxmF01@cluster0.vf0vjgc.mongodb.net/emailyprod?retryWrites=true&w=majority&appName=Cluster0
-
 if (process.env.NODE_ENV === "production") {
   // Production keys
   module.exports = require("./prod");
